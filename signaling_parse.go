2ec7c3078edd745fe23e7bc91d9b9d714a1f3e5f
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-03-22 09:40:59 +0100

    Signaling messages parsing

---
+++ signaling_parse.go
@@ -0,0 +1,70 @@
+// Signaling messages parsing
+
+package main
+
+import "strings"
+
+type SignalingMessage struct {
+	method string
+	params map[string]string
+	body   string
+}
+
+func parseSignalingMessage(raw string) SignalingMessage {
+	lines := strings.Split(raw, "\n")
+	msg := SignalingMessage{
+		method: "",
+		params: make(map[string]string),
+		body:   "",
+	}
+
+	if len(lines) > 0 {
+		msg.method = strings.ToUpper(strings.Trim(lines[0], " \n\r\t"))
+	}
+
+	var isBody bool = false
+
+	for i := 1; i < len(lines); i++ {
+		line := lines[i]
+
+		if line == "" {
+			// Found empty line
+			isBody = true
+			continue
+		}
+
+		if isBody {
+			// Body
+			if msg.body == "" {
+				msg.body = line
+			} else {
+				msg.body += "\n" + line
+			}
+		} else {
+			// Param
+			colonIndex := strings.Index(line, ":")
+			if colonIndex > 0 {
+				key := strings.ToLower(strings.Trim(line[0:colonIndex], " \n\r\t"))
+				val := strings.Trim(line[colonIndex+1:], " \n\r\t")
+				msg.params[key] = val
+			}
+		}
+	}
+
+	return msg
+}
+
+func (s SignalingMessage) serialize() string {
+	var raw string
+	raw = strings.ToUpper(s.method) + "\n"
+
+	for key, val := range s.params {
+		raw += key + ":" + val + "\n"
+	}
+
+	if s.body != "" {
+		raw += "\n" + s.body
+	}
+
+	return raw
+}
